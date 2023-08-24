94eb4f800ea418ce9c95971cd741cea35e05ea54
shibin <shibinashraf36@gmail.com>
2023-08-24 13:01:25 +0530

    about section bg removed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
                 theme === "default" ? "text-black" : "text-white"
             } flex flex-col max-w-4xl  h-[70vh] overflow-scroll mx-auto mt-4 p-4 items-center justify-center`}
         >
-            <h3 className="md:text-3xl font-bold mt-6  bg-white ">
+            <h3 className="md:text-3xl font-bold mt-6  ">
             About PEAS: Your Stress Management Companion
             </h3>
             <p className="mt-8 font-thin">Welcome to PEAS – your personal stress management companion designed to help you navigate life's challenges with ease. At PEAS, we understand the demanding pace of modern life and the toll it can take on your well-being. That's why we've crafted a collection of powerful tools to empower you in your journey towards a calmer and more balanced life.
